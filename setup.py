f5ba17dea89b95efd6fb733b675afffdcd7a9147
billy <aphexlog@gmail.com>
2023-08-10 14:37:03 -0500

    adding docs to pypip

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 from setuptools import setup, find_packages
 
+# Read the content of the README.md file
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="awsnap",
-    version="0.2",
+    version="0.3",
     description="AWS SSO Utility",
     author="Aaron West",
     author_email="aphexlog@gmail.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     entry_points={
