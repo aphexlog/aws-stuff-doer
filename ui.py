0fd834079105f4fc30bd72a7055634912b193157
billy <aphexlog@gmail.com>
2024-02-17 17:51:39 -0600

    adding root title

--- ui.py
+++ ui.py
@@ -2,19 +2,21 @@ import TermTk as ttk
 
 
 def initialize_frame_layout(root: ttk.TTk):
-    main_box = ttk.TTkFrame(parent=root, layout=ttk.TTkVBoxLayout(), border=False)
-    ttk.TTkFrame(
+    main_box = ttk.TTkFrame(
+        parent=root,
+        layout=ttk.TTkVBoxLayout(),
+        border=False,
+        color=ttk.TTkColor.fg("#FFA500"),
+    )
+    top_frame = ttk.TTkFrame(
         parent=main_box,
         layout=ttk.TTkVBoxLayout(),
     )
-    ttk.TTkFrame(
-        parent=main_box,
-        layout=ttk.TTkVBoxLayout(),
-    )
+    bottom_frame = ttk.TTkFrame(parent=main_box, layout=ttk.TTkVBoxLayout())
 
 
 def main():
-    root = ttk.TTk()
+    root = ttk.TTk(title="ASD")
     root.setLayout(ttk.TTkHBoxLayout())  # type: ignore
     initialize_frame_layout(root)
     root.mainloop()
