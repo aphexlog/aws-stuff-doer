05b274fdcf2c432986c478643b5608b9e34cadcd
billy <aphexlog@gmail.com>
2024-02-17 14:10:56 -0600

    adding some UI stuff

---
+++ ui.py
@@ -0,0 +1,26 @@
+import TermTk as ttk
+
+
+def initialize_frame_layout(root: ttk.TTk):
+    main_box = ttk.TTkFrame(
+        parent=root, layout=ttk.TTkVBoxLayout(), frame=False
+    ).setBorder(False)
+    ttk.TTkFrame(
+        parent=main_box,
+        layout=ttk.TTkVBoxLayout(),
+    )
+    ttk.TTkFrame(
+        parent=main_box,
+        layout=ttk.TTkVBoxLayout(),
+    )
+
+
+def main():
+    root = ttk.TTk()
+    root.setLayout(ttk.TTkHBoxLayout())  # type: ignore
+    initialize_frame_layout(root)
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
