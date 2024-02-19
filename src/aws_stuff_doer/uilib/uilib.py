2598aa539a00ef5687aab8bc86a7b905badfb31c
billy <aphexlog@gmail.com>
2024-02-18 19:04:30 -0600

    adding some cool ui stuff and loggin off for the night

--- src/aws_stuff_doer/uilib/uilib.py
+++ src/aws_stuff_doer/uilib/uilib.py
@@ -1,4 +1,5 @@
 from textual.app import App, ComposeResult
+from textual.events import Show
 from textual.widgets import Header, Footer
 
 
@@ -6,7 +7,22 @@ class TerminalUi(App):
     """A Textual app to manage stopwatches."""
 
     TITLE = "ASD"
-    BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
+    SUB_TITLE = " An AWS Utility to help manage your projects and sso sessions."
+    BINDINGS = [
+        ("q", "quit", "Quit"),
+        ("d", "toggle_dark", "Toggle dark mode"),
+        ("?", "show_help", "Show help"),
+    ]
+    help = """
+    ASD: An AWS Utility to help manage your projects and sso sessions.
+
+    Keybindings:
+
+        d: Toggle dark mode
+        q: Quit
+        ?: Show help
+
+    """
 
     def compose(self) -> ComposeResult:
         """Create child widgets for the app."""
@@ -15,7 +31,12 @@ class TerminalUi(App):
 
     def action_toggle_dark(self) -> None:
         """An action to toggle dark mode."""
-        self.dark = not self.dark
+        if hasattr(self, "dark"):
+            self.dark = not self.dark
+            self.refresh()
+        else:
+            self.dark = True
+            self.refresh()
 
 
 if __name__ == "__main__":
